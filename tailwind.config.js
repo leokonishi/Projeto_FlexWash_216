1b648b5f33094c0de5eba405d4cdc8d9c2a0b3a7
Gumelo023 <gustavomelopereira02@gmail.com>
2026-09-01 22:02:09 -0300

    feat: atualização do tailwind.config com a fonte padrão a ser utilizado no projeto, e as cores

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        flex: {
+          primary: '#2563EB',    // Azul principal (botões e destaques)
+          azulescuro: '#0F172A',   // Azul escuro
+          preto: '#1E293B',       // Tom escuro para fundos ou textos
+          amarelo: '#F59E0B',    // Amareko para alertas
+          branco: '#F8FAFC',      // Tom claro para fundo de telas
+          sucesso: '#10B981',    // Verde para status de conclusão
+          erro: '#EF4444',     // Vermelho para erros ou alertas
+        }
+      } ,
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'], //Fonte padrão do projeto
+      },
+    },
   },
   plugins: [],
 }
