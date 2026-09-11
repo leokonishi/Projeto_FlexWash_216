3045a10b7d6c9c2102d5feec5a2bd23c1eb2cb1d
alexlherme-a11y <alexlherme@gmail.com>
2026-09-10 22:55:32 -0300

    feat: Configuração de geração de token de sessão (JWT)

--- src/backend/bd.js
+++ src/backend/bd.js
@@ -3,14 +3,14 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'sua_senha',
+  password: 'AlE11012007!',
   database: 'FlexWash',
   port: 3306
 });
 
 db.connect((err) => {
   if (err) {
-    console.error('Erro ao conectar no MySQL:', err.message);
+    console.error('Erro ao conectar no MySQL:', err);
     return;
   }
   console.log('Conectado ao MySQL Server 2022 com sucesso!');
