63c895fda6b3fbc7966444e176c7fa9db8491418
alexlherme-a11y <alexlherme@gmail.com>
2026-09-10 23:34:36 -0300

    Adicionando o código completo dentro da rota de login

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const cadastroCliente = require('./endpoints/cadastroCliente');
+const loginCliente = require('./endpoints/loginCliente');
 
 const app = express();
 const PORTA = 8080;
@@ -10,6 +11,7 @@ app.use(express.json());
 
 // Registra o endpoint de cadastro
 app.use('/api', cadastroCliente);
+app.use('/api', loginCliente);
 
 app.listen(PORTA, () => {
   console.log(`Servidor rodando na porta ${PORTA}`);
