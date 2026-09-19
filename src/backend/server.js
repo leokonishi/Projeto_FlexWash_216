ae6f841d88211ee9a2d4761779e08d0751f2278c
Gumelo023 <gustavomelopereira02@gmail.com>
2026-09-19 11:27:09 -0300

    Corrigindo ultima atualizacao

--- src/backend/server.js
+++ src/backend/server.js
@@ -24,13 +24,13 @@ app.get('/api/protegida', verificarToken, (req, res) => {
   });
 });
 
-// Apenas executa o listen se estiver rodando localmente (fora da Vercel)
-if (process.env.NODE_ENV !== 'production');
- const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
+// Correção: Removemos o ponto e vírgula para que o listen só rode localmente
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+  });
+}
 
 // Exporta o app para a Vercel transformar em Serverless Function
 module.exports = app;
